c5eeef9d135f562eaa132b39d52824e724e6afbd
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-02-22 19:13:28 +0100

    book model added

--- booklistAPI/views.py
+++ booklistAPI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .models import Book
+
+
+@csrf_exempt
+def books(request):
+    if request.method == 'GET':
+        books = Book.objects.all().values()
+        return JsonResponse()
